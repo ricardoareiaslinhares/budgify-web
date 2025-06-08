51c6009889bbca4ab54048224c93ed490cf8a089
Ricardo Linhares <ricardolinhares.dev@gmail.com>
2025-06-08 13:35:37 +0100

    bug that get didnt work on client side, passed token from cookie woked

--- src/api-connection/config.ts
+++ src/api-connection/config.ts
@@ -11,10 +11,24 @@ export const api = axios.create({
 });
 
  api.interceptors.request.use((config) =>  {
-  const token = localStorage.getItem("authToken");
+  //const token = localStorage.getItem("authToken");
+  /* 
+  I dont know why, but the localstorage method stoped working as soon i set the cookie in api/route. now only works if i get it from the cookies
+  */
+    const token = getTokenFromCookies();
+  
   if (token) {
+
+
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 }); 
  
+
+function getTokenFromCookies() {
+  const match = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith("auth-token="));
+  return match?.split("=")[1];
+}
