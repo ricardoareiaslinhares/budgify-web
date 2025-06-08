4e3e4663983e66a9d1c01ad63fdd2059451da9eb
Ricardo Linhares <ricardolinhares.dev@gmail.com>
2025-06-08 11:15:15 +0100

    completed login and auth

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+import { API_URL, API_ROUTES } from "@/constants";
+
+export async function POST(req: Request) {
+  const body = await req.json();
+
+  const response = await fetch(`${API_URL}${API_ROUTES.login}`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body),
+  });
+
+  if (!response.ok) {
+    const text = await response.text();
+    return new NextResponse(text, { status: response.status });
+  }
+
+  const data = await response.json();
+  const token = data.data.token;
+
+const responseData = NextResponse.json(data);
+responseData.cookies.set({
+  name: "auth-token",
+  value: token,
+  httpOnly: true,
+  path: "/",
+  maxAge: 60 * 60 * 24 * 7,
+  sameSite: "lax",
+});
+return responseData;
+
+
+}
