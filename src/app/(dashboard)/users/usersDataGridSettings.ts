8f2dd8fb299b51bdaa62f6d1a312bc14aa1543d3
ricardoareiaslinhares <ricardolinhares.dev@gmail.com>
2025-06-21 10:58:53 +0100

    show ids of user

--- src/app/(dashboard)/users/usersDataGridSettings.ts
+++ src/app/(dashboard)/users/usersDataGridSettings.ts
@@ -2,12 +2,12 @@ import { GENRES } from "@/constants";
 import { DataGridColumnMapType, DataGridOptionsType } from "@/types/dataGrid";
 
 export const usersColumnMap: DataGridColumnMapType = {
-  idUser: {
-    //hidden: true,
+/*   idUser: {
+    hidden: true,
   },
   idUserGroup: {
-    //hidden: true,
-  },
+    hidden: true,
+  }, */
   profilePic: {
     hidden: true,
   },
